73ad5b7baea81a5475cda202fadc21c5c7bffdc4
Alvaro Gonzalez <alvarogonzalez@MacBook-Air-de-Alvaro.local>
2021-08-30 20:41:39 -0400

    semaforo proyect

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Traffic from './StopLigth';
+
+function App() {
+  return (
+    <div className="container text-center">
+      <Traffic></Traffic>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/StopLigth.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import './StopLigth.css';
+
+
+const Traffic = () => {
+    let [redHigh, setRed] = useState(false);
+    let [yellowHigh, setYellow] = useState(false);
+    let [greenHigh, setGreen] = useState(false);
+    const setSelected = (e) => {
+        if (e.target.id === "red") {
+            setRed(true);
+            setYellow(false);
+            setGreen(false);
+        } else if (e.target.id === "yellow") {
+            setRed(false);
+            setYellow(true);
+            setGreen(false);
+        } else if (e.target.id === "green") {
+            setRed(false);
+            setYellow(false);
+            setGreen(true);
+        }
+    }
+
+    return (
+        <div className="d-flex justify-content-center">
+            <div className="card traffic bg-dark">
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <button id="red" className={`border border-danger rounded-circle bg-danger dot ${redHigh === true ? "active-light" : ""}`}
+                                onClick={setSelected}>
+                            </button>
+                        </div>
+                        <div className="col-md-12">
+                            <button id="yellow" className={`border border-warning rounded-circle bg-warning dot ${yellowHigh === true ? "active-light" : ""}`}
+                                onClick={setSelected}></button>
+                        </div>
+                        <div className="col-md-12">
+                            <button id="green" className={`border border-success rounded-circle bg-success dot ${greenHigh === true ? "active-light" : ""}`}
+                                onClick={setSelected}>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default Traffic;
